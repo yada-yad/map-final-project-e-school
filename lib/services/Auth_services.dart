aa77068bdb583645cc544d030612dcd6d491113d
yada-yad <yad98saeed@gmail.com>
2022-01-30 15:39:33 +0300

    e-school

---
+++ lib/services/Auth_services.dart
@@ -0,0 +1,9 @@
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
+
+// ignore: camel_case_types
+class Auth_Service {
+
+  void googlesignin() async {
+  }
+}
